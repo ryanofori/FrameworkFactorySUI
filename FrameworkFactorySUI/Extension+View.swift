21ae6e68f1a9acde100fcb83dd396404687fd2ed
Ryan Ofori <ryanofori1@gmail.com>
2023-03-18 18:11:28 -0400

    Conditional modifier extension
    Added conditional modifier extension

--- FrameworkFactorySUI/Extension+View.swift
+++ FrameworkFactorySUI/Extension+View.swift
@@ -8,4 +8,25 @@
 import Foundation
 import SwiftUI
 extension View {
+    /// This allows you to apply a modfier if a condition is true
+    /// 
+    /// Source: https://designcode.io/swiftui-handbook-conditional-modifier
+    /// 
+    /// - Parameters:
+    ///   - condition: Takes in a boolean that will determine if the modifer will be represented or not.
+    ///   - transform: Closure used to pass the view that will the provided modifier when condition is true
+    /// - Returns: Only the view or the view with the applied modifier
+    /// ```swift
+    /// Text("Hi")
+    ///     .if(isFixed) { view in
+    ///         view.fixedSize(horizontal: true, vertical: false)
+    ///     }
+    /// ```
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
 }
